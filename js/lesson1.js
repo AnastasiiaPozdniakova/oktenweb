8be5a8fa1815f1854c7c5ea01b5c057546d446d3
Anastasiia Pozdniakova <nastik9097@gmail.com>
2020-11-01 21:16:51 +0200

    Lesson 3, 4

---
+++ js/lesson1.js
@@ -0,0 +1,292 @@
+/*let hello="hello";
+let owu="owu";
+let com="com";
+let ua="ua";
+let number=1;
+let num1=10;
+let num2=-999;
+let num3=123;
+let num4=3.14;
+let num5=2.7;
+let num6=16;
+let status=true;
+let status1=false;
+
+
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(number);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(status);
+alert(status1);
+
+
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(number);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num6);
+document.write(num6);
+document.write(status);
+document.write(status1);
+
+
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(status);
+console.log(status1);
+
+
+
+hello="привіт";
+owu="ову";
+com="ком";
+ua="уа";
+number=5;
+num1=100;
+num2=-39;
+num3=63;
+num4=2.71;
+num5=8.26;
+num6=6700;
+status=false;
+status1=true;
+
+
+
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(number);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(status);
+alert(status1);
+
+
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(number);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num6);
+document.write(num6);
+document.write(status);
+document.write(status1);
+
+
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(status);
+console.log(status1);
+
+
+let name="Lesya";
+let age="10";
+let form = "4";
+let page= 456;
+let book= 378;
+let year= 2020;
+
+alert(name);
+alert(age);
+alert(form);
+alert(page);
+alert(book);
+alert(year);
+
+document.write(name);
+document.write(age);
+document.write(form);
+document.write(page);
+document.write(book);
+document.write(year);
+
+
+
+console.log(name);
+console.log(age);
+console.log(form);
+console.log(page);
+console.log(book);
+console.log(year);
+
+let firsName =  prompt("What is your first name?");
+let lastName=  prompt("What is your last name?");
+let middleName = prompt("What is your middle name?");
+
+
+document.write(`<br>`);
+document.write(firstName);
+document.write(`<br>`);
+document.write(lastName);
+document.write(`<br>`);
+document.write(middleName);
+
+console.log(firsName);
+console.log(lastName);
+console.log(middleName);
+*/
+
+/* alert(firsName);
+alert(lastName);
+alert(middleName);
+
+let  person = firsName + middleName + lastName;
+console.log(person);*/
+
+
+/*let firstNameHusbend = prompt("What is your first name dear husbend?");
+let middleNameHusbend = prompt("What is your middle name dear husbend?");
+let lastNameHusbend = prompt("What is your last name dear husbend?");
+console.log(personHusbend);
+
+let firstNameDaughter = prompt(What is your first name dear daughter?);
+let middleNameDaughter = prompt(What is your middle name dear daughter?);
+let lastNameDaughter = prompt(What is your last name dear daughter?);
+console.log(personDaughter);
+
+
+let a=+promp'entert( number a');
+let b=+promp'entert( number b');
+let c=+promp'entert( number c');
+console.log(a);
+console.log(b);
+console.log(c);
+
+let a =  parseInt(prompt("Enter a"));
+let b =  parseInt(prompt("Enter b"));
+let c =  parseInt(prompt("Enter c"));
+let d =  parseInt(prompt("Enter d"));
+
+let result = a + b + c + d;
+
+console.log(result);
+
+
+let x = parseFloat(prompt("Enter float x"));
+let y = parseFloat(prompt("Enter float y"));
+let z = parseFloat(prompt("Enter float z"));
+
+let resultFloat  = x + y + z;
+
+console.log(resultFloat);
+
+x = parseFloat(prompt("Enter float x"));
+y = parseFloat(prompt("Enter float y"));
+z = parseFloat(prompt("Enter float z"));
+let  roundX = Math.round(x);
+let  roundY = Math.round(y);
+let  roundZ = Math.round(z);
+
+let resultRound =  roundX+roundY+roundZ;
+console.log(resultRound);*/
+
+/*let number1 = parseInt(prompt("Enter number1-base"));
+let number2 = parseInt(prompt("Enter number2-degree"));*/
+
+
+/*let a = 100;
+let b = '100';
+let c = true;
+let d = undefined;
+
+console.log(typeof (a),typeof (b),typeof (c),typeof (d));*/
+
+/*let a=(5<6);
+let b=(5>6);
+let c=(5>=6);
+let d=(5==6);
+let e=(10==10);
+let f=(10<=10);
+let g=(10!=10);
+let h=(10<10);
+let i=(10>10);
+let j=(123 ==='123');
+let k=(123 =='123');
+console.log(a,b,c,d,e,f,g,h,i,j,k);*/
+
+console.log(132 > 100 && 45 < 12 ); //false
+console.log(34 > 33 && 23 < 90);// true
+console.log(99 > 100 && 45 > 12 );//false
+console.log(132 > 100 || 45 < 12 );//true
+console.log(111 > 11 || 45 < 111 );//true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );//true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );// true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );//false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));//true
+console.log(!!'-1');// true
+console.log(!!-1);//true
+console.log(!!'0');//true
+console.log(!!'null');//true
+console.log(!!'undefined');//true
+console.log(!!(3/'owu'));//false
+console.log((111 > 11 || 45 < 111) ||  !!'0');//true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));//false
+
+//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
